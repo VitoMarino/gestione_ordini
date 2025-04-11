bb02c3e73aab2adcd8596a340f0ecf16c4e8f641
VitoMarino <marino.vito2001@gmail.com>
2025-04-11 09:06:58 +0200

    Indentazione e pulizia codice

--- index.php
+++ index.php
@@ -15,82 +15,75 @@
     </h1>
 
     <?php
-// Dati di connessione
-// $host = "localhost";
-// $utente = "root";
-// $password = "root";
-// $nomeDatabase = "ordini";
+    // Leggi il file .env
+    $env = file(__DIR__ . '/.env');
 
-// Leggi il file .env
-$env = file(__DIR__ . '/.env');
+    // Estrai le variabili dal file e assegnale direttamente a variabili PHP
+    // Mi scorre il file .env riga per riga
+    foreach ($env as $line) {
+        // La funzione explode divide la riga ($line) in due parti, separando la chiave (prima dell’uguale) dal valore (dopo l’uguale).
+        list($key, $value) = explode('=', $line, 2);
+        //  La parte $$key è una variabile variabile, che significa che il nome della variabile sarà preso dal valore di $key. 
+        // Ad esempio:
+        // Se $key è DB_HOST, questo significa che stai creando una variabile chiamata $DB_HOST.
+        // trim($value) rimuove eventuali spazi bianchi all’inizio o alla fine del valore.
+        $$key = trim($value); // Rimuove spazi o nuove righe
+    }
 
-// Estrai le variabili dal file e assegnale direttamente a variabili PHP
-// Mi scorre il file .env riga per riga
-foreach ($env as $line) {
-    // La funzione explode divide la riga ($line) in due parti, separando la chiave (prima dell’uguale) dal valore (dopo l’uguale).
-    list($key, $value) = explode('=', $line, 2);
-    //  La parte $$key è una variabile variabile, che significa che il nome della variabile sarà preso dal valore di $key. 
-    // Ad esempio:
-	// Se $key è DB_HOST, questo significa che stai creando una variabile chiamata $DB_HOST.
-	// trim($value) rimuove eventuali spazi bianchi all’inizio o alla fine del valore.
-    $$key = trim($value); // Rimuove spazi o nuove righe
-}
+    $host = $DB_HOST;
+    $utente = $DB_USER;
+    $password = $DB_PASS;
+    $nomeDb = $DB_NAME;
 
-$host = $DB_HOST;
-$utente = $DB_USER;
-$password = $DB_PASS;
-$nomeDb = $DB_NAME;
+    // Creazione della connessione
+    $conn = new mysqli($host, $utente, $password, $nomeDb);
 
-// Creazione della connessione
-$conn = new mysqli($host, $utente, $password, $nomeDb);
+    // Verifica della connessione
+    if ($conn->connect_error) {
+        die("Connessione fallita: " . $conn->connect_error);
+    }
 
-// Verifica della connessione
-if ($conn->connect_error) {
-    die("Connessione fallita: " . $conn->connect_error);
-}
+    // Se la connessione ha avuto successo
+    // echo "Connessione al database riuscita!";
 
-// Se la connessione ha avuto successo
-// echo "Connessione al database riuscita!";
+    //DB-------------------------------------------------------------
 
-//DB-------------------------------------------------------------
+    // Query per ottenere tutti i prodotti
+    $prodotti = "SELECT id, nome, prezzo, quantita_disponibile FROM prodotti";
+    $result = $conn->query($prodotti);
 
-// Query per ottenere tutti i prodotti
-$prodotti = "SELECT id, nome, prezzo, quantita_disponibile FROM prodotti";
-$result = $conn->query($prodotti);
-
-// num_rows è una proprietà dell’oggetto $result, che ti dà il numero di righe restituite dalla query.
-if ($result->num_rows > 0) {
-    echo
-    "<table class='table table-hover d-flex justify-content-center'>
+    // num_rows è una proprietà dell’oggetto $result, che ti dà il numero di righe restituite dalla query.
+    if ($result->num_rows > 0) {
+        echo
+        "<table class='table table-hover d-flex justify-content-center'>
         <tr>
             <th> Nome </th>
             <th> Prezzo </th>
             <th> Quantità disponibile </th>
         </tr>";
 
-    // Ciclo attraverso i risultati e stampo ogni riga della tabella
-    // La funzione fetch_assoc() è un metodo utilizzato in PHP per estrarre i dati da una query MySQL e restituirli come un array associativo.
-    while ($row = $result->fetch_assoc()) {
-        echo 
-        "<tr>
+        // Ciclo attraverso i risultati e stampo ogni riga della tabella
+        // La funzione fetch_assoc() è un metodo utilizzato in PHP per estrarre i dati da una query MySQL e restituirli come un array associativo.
+        while ($row = $result->fetch_assoc()) {
+            echo
+            "<tr>
             <td>" . $row['nome'] . " </td> 
             <td>" . $row['prezzo'] . "</td>
             <td>" . $row['quantita_disponibile'] . "</td>
             <td>" . "<i class='fa-solid fa-cart-shopping'></i>" . "</td>
         </tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "Nessun prodotto trovato";
     }
 
-    echo "</table>";
-} else {
-    echo "Nessun prodotto trovato";
-}
-
-// Chiudi la connessione
-$conn->close();
-?>
+    // Chiudi la connessione
+    $conn->close();
+    ?>
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 
 </html>
-
